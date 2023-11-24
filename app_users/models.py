0a16de6136acfd5f2cdadb3b647915e494831495
kuldeepsuther13 <ksuther43@gmail.com>
2023-11-24 15:47:56 +0500

    Add foriegn key

--- app_users/models.py
+++ app_users/models.py
@@ -14,22 +14,22 @@ class Tests(models.Model):
 
 class Sample(models.Model):
     id = models.AutoField(primary_key=True)
-    # patient = models.ForeignKey(Patient1, on_delete=models.CASCADE)
-    # test = models.ForeignKey(Tests1, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, null=True ,on_delete=models.CASCADE)
+    test = models.ForeignKey(Tests, null=True, on_delete=models.CASCADE)
     sample_type= models.CharField(max_length=150)
     collection_date = models.DateField()
 
 class Doctor(models.Model):
     id = models.AutoField(primary_key=True)
-    # patient = models.ForeignKey(Patient1, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, null=True, on_delete=models.CASCADE)
     doctor_name = models.CharField(max_length=150,null=True, blank=True)
     specialization= models.CharField(max_length=150)
     contact_details = models.CharField(max_length=250)
 
 class Result(models.Model):
     id = models.AutoField(primary_key=True)
-    # patient = models.ForeignKey(Patient1, on_delete=models.CASCADE)
-    # test = models.ForeignKey(Tests1, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient,null=True, on_delete=models.CASCADE)
+    test = models.ForeignKey(Tests, null=True, on_delete=models.CASCADE)
     result_value = models.CharField(max_length=150)
     unit = models.CharField(max_length=150)
     interpretation = models.CharField(max_length=100)
